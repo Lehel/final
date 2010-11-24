1a7364aa54771623369fd46ee7479d69f4a9d16c
Attila Juhász <juhasz@atti.la>
2010-11-24 21:37:44 +0100

    User signup complete, lesson 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,18 @@ class UsersController < ApplicationController
   
   def new
     @title = "Sign up"
+    @user = User.new
   end
-  
+  def create
+     @user = User.new(params[:user])
+     if @user.save
+       redirect_to @user, :notice => "Success"
+     else
+       @title = "Sign up"
+       render 'new'
+     end
+   end
+   
   def show
     @user = User.find(params[:id])
     @title = @user.name
