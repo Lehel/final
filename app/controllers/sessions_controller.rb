2ab76a6ae81c1cb29c91808f2d7e9bf239887bc5
Attila Juhász <juhasz@atti.la>
2010-12-01 22:04:56 +0100

    User session handling

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      redirect_to :action => :new, :error => 'Unsuccessful login'
+    else
+      sign_in user
+      redirect_to user, :notice => 'Successful login'
+    end
+  end
+
+  def destroy
+      sign_out
+      redirect_to root_path
+    end
+end
